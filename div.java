82b2973348ca5c13d9b5b1940bd70ce0d7601883
Prasan903 <110338668+Prasan903@users.noreply.github.com>
2022-09-14 15:49:42 +0530

    Add files via upload

---
+++ div.java
@@ -0,0 +1,16 @@
+package calc;
+
+public class div {
+    public int a; public int b;
+    public int div(int a,int b)
+    {
+        if(a>b)
+        {
+            return a/b;
+        }
+        else
+        {
+            return b/a;
+        }
+    }
+}
---
+++ sub.java
@@ -0,0 +1,16 @@
+package calc;
+
+public class sub {
+    public int a; public int b;
+    public int sub(int a,int b)
+    {
+        if(a>b)
+        {
+            return a-b;
+        }
+        else
+        {
+            return b-a;
+        }
+    }
+}
